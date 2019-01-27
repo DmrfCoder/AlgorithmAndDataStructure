21f469df6250ce3a95e8eef29204d34c3a264317
DmrfCoder <xuefanggang97@gmail.com>
2019-01-27 15:41:48 +0800

    从尾到头打印链表

--- Code/2.py
+++ Code/2.py
@@ -1,4 +1,8 @@
 # -*- coding:utf-8 -*-
+'''
+请实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+
+'''
 class Solution:
     # s 源字符串
     def replaceSpace(self, s):
---
+++ Code/3.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    # 返回从尾部到头部的列表值序列，例如[1,2,3]
+    def printListFromTailToHead(self, listNode):
+        # write code here
+        arrList = []
+        while (listNode != None):
+            arrList.insert(0, listNode.val)
+            listNode = listNode.next
+        return arrList
+
+
+solution = Solution()
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+print solution.printListFromTailToHead(node1)
